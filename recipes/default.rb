8cc68bd6e3f0488722e1b0be9551ba66be5ce974
Gavin Reynolds <gavin@chef.io>
2020-03-03 13:10:21 +0000

    Add reboot test config
    Signed-off-by: Gavin Reynolds <gavin@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+file "#{Chef::Config[:file_cache_path]}/reboot.txt" do
+  content 'Reboot'
+  action :create
+  notifies :request_reboot, 'reboot[Reboot Computer]'
+end
+
+reboot 'Reboot Computer' do
+  action :nothing
+  reason 'Reboot'
+end
